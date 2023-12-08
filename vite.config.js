15ffd8a14c0a1cc7dca901d5ad369ec6a1175fdf
Antonin Faure <antonin.faure@epfl.ch>
2023-12-08 17:31:49 +0100

    deploy script

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: process.env.BASE_URL || '/',
-})
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+
+  const BASE_URL = process.env.VITE_BASE_URL || ''
+
+
+  return defineConfig({
+    plugins: [vue()],
+    base: `${BASE_URL}/`
+  })
+
+} 
