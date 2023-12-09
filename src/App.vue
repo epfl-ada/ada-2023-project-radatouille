25126b2a9a078e52c77d216c78bb2f2fd5f0c31f
Antonin <antonin.faure@epfl.ch>
2023-12-09 01:09:33 +0100

    skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,180 @@
+<script setup>
+import Navbar from './Navbar.vue'
+</script>
 
 <template>
-  <div class="flex flex-col w-full h-full items-center">
-    <!-- Hero -->
-    <div
-      class="flex flex-col w-full items-center bg-[url(/banner.webp)] bg-center bg-cover bg-no-repeat min-h-[60%] justify-center text-light p-6">
-      <h1 class="text-5xl font-bold drop-shadow-lg mt-auto">Critics vs Users</h1>
-      <h3 class="text-xl font-thin mt-2 text-center drop-shadow-lg">How to explain diffences between users and critics
-        tastes in
-        movies?</h3>
-      <div class="flex flex-col items-center mt-auto text-white px-5 py-3 rounded-xl shadow-lg">
-        <span class="font-bold">A project by</span>
-        <div class="flex flex-wrap gap-1 items-center justify-center">
-          
-          <span>Antonin Faure</span>
-          <span class="hidden lg:block">&bull;</span>
-          <span>Baptiste Lecoeur</span>
-          <span class="hidden lg:block">&bull;</span>
-          <span>Enzo Palmisano</span>
-          <span class="hidden lg:block">&bull;</span>
-          <span>Jamil Maj</span>
-          <span class="hidden lg:block">&bull;</span>
-          <span>Mariella Daghfal</span>
-        </div>
+  <Navbar />
+
+  <!-- Hero -->
+  <div
+    class="flex flex-col w-full items-center bg-[url(/banner.webp)] bg-center bg-cover bg-no-repeat justify-center text-light p-6 shadow-lg min-h-[400px]">
+    <h1 class="text-5xl font-bold mt-auto drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-center">Why the New York Times
+      doesn't like
+      Marvel movies</h1>
+    <h3 class="text-xl font-thin mt-2 text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">An attempt to explain movie
+      taste differences between users and critics</h3>
+    <div class="flex flex-col items-center mt-auto px-5 py-3">
+      <span class="font-bold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">A project by</span>
+      <div class="flex flex-wrap gap-1 items-center justify-center">
+        <span class="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">Antonin Faure</span>
+        <span class="hidden lg:block drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">&bull;</span>
+        <span class="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">Baptiste Lecoeur</span>
+        <span class="hidden lg:block drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">&bull;</span>
+        <span class="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">Enzo Palmisano</span>
+        <span class="hidden lg:block drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">&bull;</span>
+        <span class="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">Jamil Maj</span>
+        <span class="hidden lg:block drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">&bull;</span>
+        <span class="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">Mariella Daghfal</span>
       </div>
     </div>
   </div>
+
+  <!-- Main -->
+  <main class="flex flex-col w-full items-center">
+    <div class="flex flex-col p-8 w-full max-w-screen-lg">
+      <section id="introduction" class="section">
+        <h2 class="text-3xl font-bold mt-5">Introduction</h2>
+        <p class="mt-2 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget
+          consequat
+          aliquam, nunc
+          ipsum
+          aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget consequat aliquam, nunc
+          ipsum
+          aliquet nunc, vitae aliquam nisl nunc vitae nisl.</p>
+
+
+        <section id="users" class="section">
+          <h3 class="text-2xl font-bold mt-8">Users</h3>
+          <p class="mt-2 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget
+            consequat
+            aliquam, nunc
+            ipsum
+            aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget consequat aliquam, nunc
+            ipsum
+            aliquet nunc, vitae aliquam nisl nunc vitae nisl.</p>
+        </section>
+
+        <section id="critics" class="section">
+          <h3 class="text-2xl font-bold mt-8">Critics</h3>
+          <p class="mt-2 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget
+            consequat
+            aliquam, nunc
+            ipsum
+            aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget consequat aliquam, nunc
+            ipsum
+            aliquet nunc, vitae aliquam nisl nunc vitae nisl.</p>
+        </section>
+
+        <section id="different-tastes" class="section">
+          <h3 class="text-2xl font-bold mt-8">Different tastes</h3>
+          <p class="mt-2 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget
+            consequat
+            aliquam, nunc
+            ipsum
+            aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget consequat aliquam, nunc
+            ipsum
+            aliquet nunc, vitae aliquam nisl nunc vitae nisl.</p>
+        </section>
+      </section>
+
+      <section id="exploration" class="section">
+        <h2 class="text-3xl font-bold mt-8">Exploration</h2>
+
+        <section id="genres" class="section">
+          <h3 class="text-2xl font-bold mt-8">Genres</h3>
+          <p class="mt-2 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget
+            consequat
+            aliquam, nunc
+            ipsum
+            aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget consequat aliquam, nunc
+            ipsum
+            aliquet nunc, vitae aliquam nisl nunc vitae nisl.</p>
+        </section>
+
+        <section id="awards" class="section">
+          <h3 class="text-2xl font-bold mt-8">Awards</h3>
+          <p class="mt-2 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget
+            consequat
+            aliquam, nunc
+            ipsum
+            aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget consequat aliquam, nunc
+            ipsum
+            aliquet nunc, vitae aliquam nisl nunc vitae nisl.</p>
+        </section>
+
+        <section id="countries" class="section">
+          <h3 class="text-2xl font-bold mt-8">Countries</h3>
+          <p class="mt-2 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget
+            consequat
+            aliquam, nunc
+            ipsum
+            aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget consequat aliquam, nunc
+            ipsum
+            aliquet nunc, vitae aliquam nisl nunc vitae nisl.</p>
+        </section>
+
+        <section id="release-year" class="section">
+          <h3 class="text-2xl font-bold mt-8">Release Year</h3>
+          <p class="mt-2 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget
+            consequat
+            aliquam, nunc
+            ipsum
+            aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget consequat aliquam, nunc
+            ipsum
+            aliquet nunc, vitae aliquam nisl nunc vitae nisl.</p>
+        </section>
+
+        <section id="runtime" class="section">
+          <h3 class="text-2xl font-bold mt-8">Runtime</h3>
+          <p class="mt-2 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget
+            consequat
+            aliquam, nunc
+            ipsum
+            aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget consequat aliquam, nunc
+            ipsum
+            aliquet nunc, vitae aliquam nisl nunc vitae nisl.</p>
+        </section>
+
+        <section id="actors" class="section">
+          <h3 class="text-2xl font-bold mt-8">Actors</h3>
+          <p class="mt-2 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget
+            consequat
+            aliquam, nunc
+            ipsum
+            aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget consequat aliquam, nunc
+            ipsum
+            aliquet nunc, vitae aliquam nisl nunc vitae nisl.</p>
+        </section>
+
+        <section id="plot" class="section">
+          <h3 class="text-2xl font-bold mt-8">Plot</h3>
+          <p class="mt-2 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget
+            consequat
+            aliquam, nunc
+            ipsum
+            aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget consequat aliquam, nunc
+            ipsum
+            aliquet nunc, vitae aliquam nisl nunc vitae nisl.</p>
+        </section>
+      </section>
+
+      <section id="conclusion" class="section">
+        <h2 class="text-2xl font-bold mt-8">Conclusion</h2>
+        <p class="mt-2 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget
+          consequat
+          aliquam, nunc
+          ipsum
+          aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget consequat aliquam, nunc
+          ipsum
+          aliquet nunc, vitae aliquam nisl nunc vitae nisl.</p>
+      </section>
+    </div>
+  </main>
 </template>
+
+<style scoped>
+.section {
+  padding-top: 50px;
+  margin-top: -50px;
+}
+</style>
---
+++ src/Navbar.vue
@@ -0,0 +1,151 @@
+<template>
+    <nav class="h-14 flex bg-dark text-light items-center justify-end sticky top-0 left-0 w-full z-10">
+        <button class="cursor-pointer w-14 h-14 flex items-center justify-center" title="Open menu"
+            @click="() => setIsMobileMenuOpen(true)">
+            <Bars3Icon class="w-6 h-6" />
+        </button>
+    </nav>
+
+    <!-- Mobile menu -->
+    <TransitionRoot appear :show="isMobileMenuOpen" as="template">
+        <Dialog as="div" @close="setIsMobileMenuOpen">
+            <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0" enter-to="opacity-100"
+                leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0">
+                <div class="fixed inset-0 bg-black/50" />
+            </TransitionChild>
+
+            <div class="fixed inset-0 overflow-y-auto overflow-x-hidden z-20">
+                <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0 translate-x-full"
+                    enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100"
+                    leave-to="opacity-0 translate-x-full">
+                    <DialogPanel class="h-full ml-16">
+                        <div class="bg-dark text-light h-full w-full overflow-y-auto p-8">
+                            <button class="bg-slate-500 rounded-full p-3 text-white fixed top-8 right-8 hover:bg-slate-600"
+                                title="Close menu" @click="() => setIsMobileMenuOpen(false)">
+                                <XMarkIcon class="w-6 h-6" />
+                            </button>
+
+                            <div v-for="section in sections" :key="section.id" class="mt-4">
+                                <h3 class="text-3xl font-medium mt-4">
+                                    <a class="flex gap-3 items-center py-2 hover:text-slate-400"
+                                        :href="section.mainMenuLink?.link" @click="() => setIsMobileMenuOpen(false)">
+                                        <span>{{ section.mainMenuLink?.title }}</span>
+                                        <ChevronRightIcon class="w-6 h-6" />
+                                    </a>
+                                </h3>
+                                <a v-for="link in section.secondaryMenuLinks" :key="link.id"
+                                    class="text-xl block py-2 hover:text-slate-400" :href="link.link"
+                                    @click="() => setIsMobileMenuOpen(false)">
+                                    {{ link.title }}
+                                </a>
+                            </div>
+                        </div>
+                    </DialogPanel>
+                </TransitionChild>
+            </div>
+        </Dialog>
+    </TransitionRoot>
+</template>
+  
+<script setup>
+import {
+    TransitionRoot,
+    TransitionChild,
+    Dialog,
+    DialogPanel
+} from '@headlessui/vue'
+
+import { Bars3Icon, ChevronRightIcon, XMarkIcon } from '@heroicons/vue/24/solid'
+
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const isMobileMenuOpen = ref(false)
+
+const sections = [{
+    id: 1,
+    mainMenuLink: {
+        title: 'Introduction',
+        link: '#introduction'
+    },
+    secondaryMenuLinks: [{
+        id: 1,
+        title: 'Users',
+        link: '#users'
+    },
+    {
+        id: 2,
+        title: 'Critics',
+        link: '#critics'
+    }, {
+        id: 3,
+        title: 'Different tastes',
+        link: '#different-tastes'
+    }
+    ]
+},
+{
+    id: 2,
+    mainMenuLink: {
+        title: 'Exploration',
+        link: '#exploration'
+    },
+    secondaryMenuLinks: [{
+        id: 1,
+        title: 'Genres',
+        link: '#genres'
+    }, {
+        id: 2,
+        title: 'Awards',
+        link: '#awards'
+    },
+    {
+        id: 3,
+        title: 'Countries',
+        link: '#countries'
+    }, {
+        id: 4,
+        title: 'Release Year',
+        link: '#release-year'
+    }, {
+        id: 5,
+        title: 'Runtime',
+        link: '#runtime'
+    }, {
+        id: 6,
+        title: 'Actors',
+        link: '#actors'
+    }, {
+        id: 7,
+        title: 'Plot',
+        link: '#plot'
+    }
+    ]
+},
+{
+    id: 3,
+    mainMenuLink: {
+        title: 'Conclusion',
+        link: '#conclusion'
+    },
+    secondaryMenuLinks: []
+
+}]
+
+function setIsMobileMenuOpen(value) {
+    isMobileMenuOpen.value = value
+}
+
+onMounted(() => {
+    window.addEventListener('resize', handleWindowSizeChange)
+    handleWindowSizeChange()
+})
+onUnmounted(() => {
+    window.removeEventListener('resize', handleWindowSizeChange)
+})
+const handleWindowSizeChange = () => {
+    if (window.innerWidth >= 768) { // Tailwind’s md breakpoint
+        setIsMobileMenuOpen(false)
+    }
+}
+</script>
+  
